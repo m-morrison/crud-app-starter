3015687098e3b81d719fc2d2b7978591eac2fa62
Houchens <perry.houchens@accenturefederal.com>
2018-03-24 16:26:21 -0400

    updates api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+//axios for making http request
+const axios = require('axios');
+//not using a real database
+const API = 'https://jsonplaceholder.typicode.com';
+
 // GET API listing
 router.get('/', (req, res) => {
     res.send('congrats, your api works');
 });
+
+// Get all todos
+router.get('/todos', (req, res) => {
+    // Get todos from the mock api
+    // This should ideally be replaced with a service that connects to MongoDB
+    axios.get(`${API}/todos`)
+        .then(todos => {
+            res.status(200).send(todos.data);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+        });
+});
+
 
 module.exports = router;
